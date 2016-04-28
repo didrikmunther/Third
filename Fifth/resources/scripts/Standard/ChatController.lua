cba4973e4a4984624350a4558ab00653b519fe8b
Malaxiz <malaxiz.main@gmail.com>
2016-04-28 13:17:39 +0200

    Revert "Merge pull request #7 from Azinum/patch-5"
    This reverts commit e7ab90ae01d78f8240fa8b886635246e5dbe02df, reversing
    changes made to 0fe7c7550e25d330fdf942d612319a6662bb409c.

--- Fifth/resources/scripts/Standard/ChatController.lua
+++ Fifth/resources/scripts/Standard/ChatController.lua
@@ -7,13 +7,11 @@ local ChatController = class (
 
         self.isTyping = false
         self.buffer = ""
-        self.currentChatIndex = 1
-        self.chatArray = {""}
-                              
+
         self.width = 50
         self.initWidth = self.width
         self.height = 40
-        self.maxBufferSize = 1024
+        self.maxBufferSize = 100000000
                               
         self.shift = false
     end
@@ -85,23 +83,11 @@ function ChatController:onTextInput(input)
             self.buffer = self.buffer .. input
         end
     end
+
 end
 
 function ChatController:onEvent(key, keyDown)
     if(not keyDown) then do return end end
-
-    if(key == KeyCode._ARROW_DOWN) then
-        if(table.getn(self.chatArray) > 1 and self.currentChatIndex > 1 and self.chatArray[self.currentChatIndex] ~= nil) then
-            self.currentChatIndex = self.currentChatIndex - 1
-            self.buffer = self.chatArray[self.currentChatIndex]
-        end
-    end
-    if(key == KeyCode._ARROW_UP) then
-        if(self.currentChatIndex < table.getn(self.chatArray) and self.chatArray[self.currentChatIndex] ~= nil) then
-            self.currentChatIndex = self.currentChatIndex + 1
-            self.buffer = self.chatArray[self.currentChatIndex]
-        end
-    end
 
     if(key == KeyCode._RETURN or key == KeyCode._SLASH) then
         if(key == KeyCode._SLASH) then
@@ -115,7 +101,6 @@ function ChatController:onEvent(key, keyDown)
             if(toSay ~= "") then
                 self.component.instance.player:say(toSay, "TESTFONT", ChatBubbleType.SAY)
             end
-            table.insert(self.chatArray, self.buffer)
             self.buffer = ""
             self.component.instance.game.ignoreEvents = false
             do return end
